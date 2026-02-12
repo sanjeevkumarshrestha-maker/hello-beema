a76b90c8f6eee0220a670d239ca362b765323152
sanjeevkumarshrestha-maker <sanjeevkumarshrestha@gmail.com>
2026-02-12 22:30:09 +0545

    Update supabase anon key update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Next.js requires the NEXT_PUBLIC_ prefix to use these on the frontend
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
